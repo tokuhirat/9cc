ea42111ee2069a244ddff0164f9a1ab2d668afdc
Tokuhira <sgsnknch@gmail.com>
2023-11-04 18:09:00 +0900

    ファイル分割とMakefileの変更

---
+++ 9cc.h
@@ -0,0 +1,62 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED,  // 記号
+    TK_NUM,  // 整数トークン
+    TK_EOF,  // 入力の終わりを表すトークン
+} TokenKind;
+
+// トークン型
+typedef struct Token Token;
+struct Token {
+    TokenKind kind;  // トークンの型
+    Token *next;  // 次の入力トークン
+    int val;  // kindがTK_NUMの場合、その数値
+    char *str;  // トークン文字列
+    int len;  // トークンの長さ
+};
+
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD,  // +
+    ND_SUB,  // -
+    ND_MUL,  // *
+    ND_DIV,  // +
+    ND_NUM,  // 整数
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
+} NodeKind;
+
+// 抽象構文木のノードの型
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // ノードの型
+    Node *lhs;  // 左辺
+    Node *rhs;  // 右辺
+    int val;  // kindがND_NUMの場合のみ使う
+};
+
+// 現在着目しているトークン
+extern Token *token;
+
+// 入力プログラム
+extern char *user_input;
+
+void error_at(char *loc, char *fmt, ...);
+Token *tokenize(char *p);
+void gen(Node *node);
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
