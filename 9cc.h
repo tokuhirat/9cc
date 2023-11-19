eccb86147e6be0b281c7b96bd35d104d3805d0c3
Tokuhira <sgsnknch@gmail.com>
2023-11-19 21:26:59 +0900

    for文の実装

--- 9cc.h
+++ 9cc.h
@@ -72,6 +72,7 @@ typedef enum {
     ND_ASSIGN,     // =
     ND_RETURN,     // return
     ND_IF,         // if
+    ND_FOR,         // for
     ND_BLOCK,      // { ... }
     ND_EXPR_STMT,  // Expression statement
     ND_VAR,        // 変数
@@ -86,12 +87,12 @@ struct Node {
     Node *lhs;      // 左辺
     Node *rhs;      // 右辺
 
-    // "if" statement
+    // "if" or "for" statement
     Node *cond;
     Node *then;
     Node *els;
-
-
+    Node *init;
+    Node *inc;
 
     // Block
     Node *body;
