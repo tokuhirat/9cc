4be4eda5b6a748427a39811dfcbf733fb0b55cde
Tokuhira <sgsnknch@gmail.com>
2023-11-24 19:59:02 +0900

    chibccを参考にリファクタリング

--- codegen.c
+++ codegen.c
@@ -2,6 +2,7 @@
 
 static int depth;
 static char *argreg[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
+static Function *current_fn;
 
 static int count(void) {
     static int i = 1;
@@ -107,32 +108,32 @@ static void gen_expr(Node *node) {
     error_tok(node->tok, "invalid expression");
 }
 
-static void gen_stmt(Node *node, char *name) {
+static void gen_stmt(Node *node) {
     switch (node->kind) {
     case ND_IF: {
         int c = count();
         gen_expr(node->cond);
         printf("  cmp rax, 0\n");
         printf("  je .L.else.%d\n", c);
-        gen_stmt(node->then, name);
+        gen_stmt(node->then);
         printf("  jmp .L.end.%d\n", c);
         printf(".L.else.%d:\n", c);
         if (node->els)
-            gen_stmt(node->els, name);
+            gen_stmt(node->els);
         printf(".L.end.%d:\n", c);
         return;
     }
     case ND_FOR: {
         int c = count();
         if (node->init)
-            gen_stmt(node->init, name);
+            gen_stmt(node->init);
         printf(".L.begin.%d:\n", c);
         if (node->cond) {
             gen_expr(node->cond);
             printf("  cmp rax, 0\n");
             printf("  je .L.end.%d\n", c);
         }
-        gen_stmt(node->then, name);
+        gen_stmt(node->then);
         if (node->inc)
             gen_expr(node->inc);
         printf("  jmp .L.begin.%d\n", c);
@@ -141,11 +142,11 @@ static void gen_stmt(Node *node, char *name) {
     }
     case ND_BLOCK:
         for (Node *n = node->body; n; n= n->next)
-            gen_stmt(n, name);
+            gen_stmt(n);
         return;
     case ND_RETURN:
         gen_expr(node->lhs);
-        printf("  jmp .L.return.%s\n", name);
+        printf("  jmp .L.return.%s\n", current_fn->name);
         return;
     case ND_EXPR_STMT:
         gen_expr(node->lhs);
@@ -155,39 +156,39 @@ static void gen_stmt(Node *node, char *name) {
     error_tok(node->tok, "invalud statement");
 }
 
-static void gen_func(Function *fn) {
-    char *name = fn->name;
-    
-    printf("%s:\n", name);
-    // Prologue
-    printf("  push rbp\n");
-    printf("  mov rbp, rsp\n");
-    printf("  sub rsp, %d\n", fn->stack_size);
-
-    gen_stmt(fn->body, name);
-    assert(depth == 0);
-
-    printf(".L.return.%s:\n", name);
-    printf("  mov rsp, rbp\n");
-    printf("  pop rbp\n");
-    printf("  ret\n");
-}
-
 static void assign_lvar_offsets(Function *prog) {
-    int offset = 0;
-    for (Obj *var = prog->locals; var; var = var->next) {
-        offset += 8;
-        var->offset = -offset;
+    for (Function *fn = prog; fn; fn = fn->next) {
+        int offset = 0;
+        for (Obj *var = fn->locals; var; var = var->next) {
+            offset += 8;
+            var->offset = -offset;
+        }
+        fn->stack_size = align_to(offset, 16);   
     }
-    prog->stack_size = align_to(offset, 16);
 }
 
 void codegen(Function *prog) {
     assign_lvar_offsets(prog);
 
     printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
 
-    for (Function *fn = prog; fn; fn = fn->next)
-        gen_func(fn);
+    for (Function *fn = prog; fn; fn = fn->next) {
+        printf("  .globl %s\n", fn->name);
+        printf("%s:\n", fn->name);
+        current_fn = fn;
+        
+        // Prologue
+        printf("  push rbp\n");
+        printf("  mov rbp, rsp\n");
+        printf("  sub rsp, %d\n", fn->stack_size);
+
+        gen_stmt(fn->body);
+        assert(depth == 0);
+
+        // Epilogue
+        printf(".L.return.%s:\n", fn->name);
+        printf("  mov rsp, rbp\n");
+        printf("  pop rbp\n");
+        printf("  ret\n");
+    }
 }
